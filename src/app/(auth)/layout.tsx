23d46904444159423c52dcc80be87cf2223adc1e
Hamid <hamidali006789@gmail.com>
2024-05-01 09:18:35 +0500

    login form and login server action with supabase

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface AuthLayoutProps {
+  children: React.ReactNode;
+}
+
+const AuthLayout: React.FC<AuthLayoutProps> = ({ children }) => {
+  return (
+    <div
+      className="
+      h-screen
+      p-6 flex 
+      justify-center"
+    >
+      {children}
+    </div>
+  );
+};
+
+export default AuthLayout;
